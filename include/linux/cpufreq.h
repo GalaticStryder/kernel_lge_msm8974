8f4f8f7feae415554a7efda3a16bcbf1dc25d1f7
Viresh Kumar <viresh.kumar@linaro.org>
2013-06-19 10:16:55 +0530

    cpufreq: make sure frequency transitions are serialized
    Whenever we are changing frequency of a cpu, we are calling PRECHANGE and
    POSTCHANGE notifiers. They must be serialized. i.e. PRECHANGE or POSTCHANGE
    shouldn't be called twice contiguously.
    
    This can happen due to bugs in users of __cpufreq_driver_target() or actual
    cpufreq drivers who are sending these notifiers.
    
    This patch adds some protection against this. Now, we keep track of the last
    transaction and see if something went wrong.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Git-commit: 7c30ed532cf798a8d924562f2f44d03d7652f7a7
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -122,6 +122,7 @@ struct cpufreq_policy {
 
 	struct kobject		kobj;
 	struct completion	kobj_unregister;
+	bool			transition_ongoing; /* Tracks transition status */
 };
 
 #define CPUFREQ_ADJUST			(0)
