3073dda7b40fffdc4d391db1e843c043b1904e50
Knut Petersen <Knut_Petersen@t-online.de>
2013-09-25 14:29:37 +0200

    perf: Enforce 1 as lower limit for perf_event_max_sample_rate
    commit 723478c8a471403c53cf144999701f6e0c4bbd11 upstream.
    
    /proc/sys/kernel/perf_event_max_sample_rate will accept
    negative values as well as 0.
    
    Negative values are unreasonable, and 0 causes a
    divide by zero exception in perf_proc_update_handler.
    
    This patch enforces a lower limit of 1.
    
    Signed-off-by: Knut Petersen <Knut_Petersen@t-online.de>
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/5242DB0C.4070005@t-online.de
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Cc: Weng Meiling <wengmeiling.weng@huawei.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -989,6 +989,7 @@ static struct ctl_table kern_table[] = {
 		.maxlen		= sizeof(sysctl_perf_event_sample_rate),
 		.mode		= 0644,
 		.proc_handler	= perf_proc_update_handler,
+		.extra1		= &one,
 	},
 #endif
 #ifdef CONFIG_KMEMCHECK
