e77d2b32d7b5aeeb051806e384c48f5cfe39f431
Terje Bergstrom <tbergstrom@nvidia.com>
2013-10-09 15:21:42 +0300

    PM / QoS: Add notifier for flags
    dev_pm_qos has a notifier for DEV_PM_QOS_LATENCY. Add a similar
    notifier for DEV_PM_QOS_FLAGS.
    
    Bug 1364240
    
    Change-Id: Ica4c58708855938818a1e75896503b9023b96573
    Signed-off-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Reviewed-on: http://git-master/r/288810

--- drivers/base/power/qos.c
+++ drivers/base/power/qos.c
@@ -150,6 +150,9 @@ static int apply_constraint(struct dev_pm_qos_request *req,
 	case DEV_PM_QOS_FLAGS:
 		ret = pm_qos_update_flags(&qos->flags, &req->data.flr,
 					  action, value);
+		blocking_notifier_call_chain(&dev_pm_notifiers,
+					     (unsigned long)value,
+					     req);
 		break;
 	default:
 		ret = -EINVAL;
@@ -169,7 +172,7 @@ static int dev_pm_qos_constraints_allocate(struct device *dev)
 {
 	struct dev_pm_qos *qos;
 	struct pm_qos_constraints *c;
-	struct blocking_notifier_head *n;
+	struct blocking_notifier_head *n, *fn;
 
 	qos = kzalloc(sizeof(*qos), GFP_KERNEL);
 	if (!qos)
@@ -182,6 +185,14 @@ static int dev_pm_qos_constraints_allocate(struct device *dev)
 	}
 	BLOCKING_INIT_NOTIFIER_HEAD(n);
 
+	fn = kzalloc(sizeof(*fn), GFP_KERNEL);
+	if (!fn) {
+		kfree(n);
+		kfree(qos);
+		return -ENOMEM;
+	}
+	BLOCKING_INIT_NOTIFIER_HEAD(fn);
+
 	c = &qos->latency;
 	plist_head_init(&c->list);
 	c->target_value = PM_QOS_DEV_LAT_DEFAULT_VALUE;
@@ -190,6 +201,7 @@ static int dev_pm_qos_constraints_allocate(struct device *dev)
 	c->notifiers = n;
 
 	INIT_LIST_HEAD(&qos->flags.list);
+	qos->flags.notifiers = fn;
 
 	spin_lock_irq(&dev->power.lock);
 	dev->power.qos = qos;
@@ -473,6 +485,10 @@ int dev_pm_qos_add_notifier(struct device *dev, struct notifier_block *notifier)
 	if (!ret)
 		ret = blocking_notifier_chain_register(
 				dev->power.qos->latency.notifiers, notifier);
+
+	if (!ret)
+		ret = blocking_notifier_chain_register(
+				dev->power.qos->flags.notifiers, notifier);
 
 	mutex_unlock(&dev_pm_qos_mtx);
 	return ret;
