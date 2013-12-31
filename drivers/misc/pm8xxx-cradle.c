68489b365c42f4017cff20d3a6e99b778ad62cd0
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-12-31 04:33:28 +0000

    BU52031NVX: Add lid events to support the smartcover
    Conflicts:
    	drivers/misc/pm8xxx-cradle.c
    
    Change-Id: Ifa304773a134aac9a88c8c7dbd3fa01c10f5bf72

--- drivers/misc/pm8xxx-cradle.c
+++ drivers/misc/pm8xxx-cradle.c
@@ -6,6 +6,7 @@
 
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/input.h>
 #include <linux/kernel.h>
 #include <linux/errno.h>
 #include <linux/interrupt.h>
@@ -44,6 +45,7 @@ struct pm8xxx_cradle {
 
 static struct workqueue_struct *cradle_wq;
 static struct pm8xxx_cradle *cradle;
+static struct input_dev  *cradle_input;
 
 #if defined(CONFIG_TOUCHSCREEN_ATMEL_S540)
 static int is_smart_cover_closed = 0; /* check status of smart cover to resize quick window area */
@@ -108,6 +110,11 @@ static void boot_cradle_det_func(void)
 #if defined(CONFIG_TOUCHSCREEN_ATMEL_S540)
 	is_smart_cover_closed = cradle->pouch;
 #endif
+
+        input_report_switch(cradle_input, SW_LID, 
+                cradle->state == SMARTCOVER_POUCH_OPENED ? 0 : 1);
+        input_sync(cradle_input);
+
 }
 
 #if defined CONFIG_HALLIC_PEN
@@ -229,6 +236,9 @@ static void pm8xxx_pouch_work_func(struct work_struct *work)
 		wake_lock_timeout(&cradle->wake_lock, msecs_to_jiffies(3000));
 		switch_set_state(&cradle->sdev, cradle->state);
 		printk("%s : [Cradle] pouch value is %d\n", __func__ , state);
+                input_report_switch(cradle_input, SW_LID, 
+                        cradle->state == SMARTCOVER_POUCH_OPENED ? 0 : 1);
+                input_sync(cradle_input);
 	}
 	else {
 		spin_unlock_irqrestore(&cradle->lock, flags);
@@ -620,10 +630,40 @@ static struct platform_driver pm8xxx_cradle_driver = {
 	},
 };
 
+static int cradle_input_device_create(void){
+        int err = 0;
+
+        cradle_input = input_allocate_device();
+        if (!cradle_input) {
+                err = -ENOMEM;
+                goto exit;
+        }
+
+        cradle_input->name = "smartcover";
+        cradle_input->phys = "/dev/input/smartcover";
+
+        set_bit(EV_SW, cradle_input->evbit);
+        set_bit(SW_LID, cradle_input->swbit);
+
+        err = input_register_device(cradle_input);
+        if (err) {
+                goto exit_free;
+        }
+        return 0;
+
+exit_free:
+        input_free_device(cradle_input);
+        cradle_input = NULL;
+exit:
+        return err;
+
+}
+
 static int __init pm8xxx_cradle_init(void)
 {
-	cradle_wq = create_singlethread_workqueue("cradle_wq");
-       printk(KERN_ERR "cradle init \n");
+        cradle_input_device_create();
+        cradle_wq = create_singlethread_workqueue("cradle_wq");
+        printk(KERN_ERR "cradle init \n");
 	if (!cradle_wq)
 		return -ENOMEM;
 	return platform_driver_register(&pm8xxx_cradle_driver);
@@ -634,6 +674,7 @@ static void __exit pm8xxx_cradle_exit(void)
 {
 	if (cradle_wq)
 		destroy_workqueue(cradle_wq);
+        input_unregister_device(cradle_input);
 	platform_driver_unregister(&pm8xxx_cradle_driver);
 }
 module_exit(pm8xxx_cradle_exit);
