8752a5bdb450eaa4f3fe9613a64f60b3dd7a74ea
Pranav Vashi <neobuddy89@gmail.com>
2014-06-02 01:58:46 +0530

    workqueue: Remove panic
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -1893,7 +1893,6 @@ __acquires(&gcwq->lock)
 		printk(KERN_ERR "    last function: ");
 		print_symbol("%s\n", (unsigned long)f);
 		debug_show_held_locks(current);
-		BUG_ON(PANIC_CORRUPTION);
 		dump_stack();
 	}
 
