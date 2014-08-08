4d62baadcbd275afe4519f1f6b61e7ec01f51d24
Paul Reioux <reioux@gmail.com>
2014-08-07 23:46:40 -0700

    Sound Control: LG G3 Speaker Amp control
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- sound/soc/codecs/sound_control_3_gpl.c
+++ sound/soc/codecs/sound_control_3_gpl.c
@@ -232,9 +232,9 @@ static ssize_t speaker_gain_show(struct kobject *kobj,
 {
         return sprintf(buf, "%u %u\n",
 			taiko_read(fauxsound_codec_ptr,
-				TAIKO_A_CDC_RX7_VOL_CTL_B2_CTL),
+				TAIKO_A_CDC_RX3_VOL_CTL_B2_CTL),
 			taiko_read(fauxsound_codec_ptr,
-				TAIKO_A_CDC_RX7_VOL_CTL_B2_CTL));
+				TAIKO_A_CDC_RX4_VOL_CTL_B2_CTL));
 
 }
 
@@ -247,9 +247,9 @@ static ssize_t speaker_gain_store(struct kobject *kobj,
 
 	if (calc_checksum(lval, rval, chksum)) {
 		taiko_write(fauxsound_codec_ptr,
-			TAIKO_A_CDC_RX7_VOL_CTL_B2_CTL, lval);
+			TAIKO_A_CDC_RX3_VOL_CTL_B2_CTL, lval);
 		taiko_write(fauxsound_codec_ptr,
-			TAIKO_A_CDC_RX7_VOL_CTL_B2_CTL, rval);
+			TAIKO_A_CDC_RX4_VOL_CTL_B2_CTL, rval);
 	}
 	return count;
 }
