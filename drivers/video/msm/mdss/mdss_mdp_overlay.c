713aaab337fdd32780a9ec64734c7bbd409042e3
Jeevan Shriram <jshriram@codeaurora.org>
2014-09-03 13:01:48 -0700

    msm: mdss: add support to parse dynamic refresh rate range
    DSI panel dynamic refresh rate range varies from panel to panel.
    Add support to parse dynamic refresh range from the panel device
    tree. If the range is not specified, use the default range from
    30 to max panel supported rate.
    
    Change-Id: Iac5adebe187bdc7abd4e82464de81305862df794
    Signed-off-by: Jeevan Shriram <jshriram@codeaurora.org>
    Signed-off-by: Baldev Sahu <bsahu@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -2001,20 +2001,15 @@ static ssize_t dynamic_fps_sysfs_wta_dfps(struct device *dev,
 	}
 
 	mutex_lock(&mdp5_data->dfps_lock);
-
-#ifdef CONFIG_LGE_DEVFREQ_DFPS
-	if (dfps < 38) {
-		pr_err("Unsupported FPS. Configuring to min_fps = 38\n");
-		dfps = 38;
-#else
-	if (dfps < 30) {
-		pr_err("Unsupported FPS. Configuring to min_fps = 30\n");
-		dfps = 30;
-#endif
-		rc = mdss_mdp_ctl_update_fps(mdp5_data->ctl, dfps);
-	} else if (dfps > 60) {
-		pr_err("Unsupported FPS. Configuring to max_fps = 60\n");
-		dfps = 60;
+	if (dfps < pdata->panel_info.min_fps) {
+		pr_err("Unsupported FPS. min_fps = %d\n",
+				pdata->panel_info.min_fps);
+		mutex_unlock(&mdp5_data->dfps_lock);
+		return -EINVAL;
+	} else if (dfps > pdata->panel_info.max_fps) {
+		pr_warn("Unsupported FPS. Configuring to max_fps = %d\n",
+				pdata->panel_info.max_fps);
+		dfps = pdata->panel_info.max_fps;
 		rc = mdss_mdp_ctl_update_fps(mdp5_data->ctl, dfps);
 	} else {
 		rc = mdss_mdp_ctl_update_fps(mdp5_data->ctl, dfps);
