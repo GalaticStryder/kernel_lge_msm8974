6598f0c6abd3bac38300b24c2a693265cb3d3777
UpInTheAir <tyson_cj@hotmail.com>
2014-10-03 12:22:13 +0700

    mm: don't wait for congestion when mem is high [Oppo]
    This fixes a huge IOWAIT issue when memory usage is high.

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -1293,7 +1293,7 @@ shrink_inactive_list(unsigned long nr_to_scan, struct mem_cgroup_zone *mz,
 	struct lruvec *lruvec = mem_cgroup_zone_lruvec(zone, mz->mem_cgroup);
 
 	while (unlikely(too_many_isolated(zone, file, sc))) {
-		congestion_wait(BLK_RW_ASYNC, HZ/10);
+		congestion_wait_kswapd(BLK_RW_ASYNC, HZ/10);
 
 		/* We are about to die and free our memory. Return now. */
 		if (fatal_signal_pending(current))
