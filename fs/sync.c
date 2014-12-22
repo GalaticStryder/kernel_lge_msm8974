36ddaba3a81932e4c45cd8198cb540de9f05057b
savoca <adeddo27@gmail.com>
2014-12-22 17:10:00 +0000

    fs: sync: Control from configs
     - This is needed to build stock LG kernels out of furnace source (excl. kcal)

--- fs/sync.c
+++ fs/sync.c
@@ -7,7 +7,9 @@
 #include <linux/fs.h>
 #include <linux/slab.h>
 #include <linux/export.h>
+#ifdef CONFIG_FS_SYNC
 #include <linux/module.h>
+#endif
 #include <linux/namei.h>
 #include <linux/sched.h>
 #include <linux/writeback.h>
@@ -18,8 +20,10 @@
 #include <linux/backing-dev.h>
 #include "internal.h"
 
+#ifdef CONFIG_FS_SYNC
 bool fsync_enabled = true;
 module_param(fsync_enabled, bool, 0755);
+#endif
 
 #define VALID_FLAGS (SYNC_FILE_RANGE_WAIT_BEFORE|SYNC_FILE_RANGE_WRITE| \
 			SYNC_FILE_RANGE_WAIT_AFTER)
@@ -141,9 +145,11 @@ SYSCALL_DEFINE1(syncfs, int, fd)
 	struct super_block *sb;
 	int ret;
 	int fput_needed;
-	
+
+#ifdef CONFIG_FS_SYNC
 	if (!fsync_enabled)
 		return 0;
+#endif
 
 	file = fget_light(fd, &fput_needed);
 	if (!file)
@@ -171,9 +177,11 @@ SYSCALL_DEFINE1(syncfs, int, fd)
  */
 int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 {
+#ifdef CONFIG_FS_SYNC
 	if (!fsync_enabled)
 		return 0;
-		
+#endif
+
 	if (!file->f_op || !file->f_op->fsync)
 		return -EINVAL;
 	return file->f_op->fsync(file, start, end, datasync);
@@ -190,9 +198,11 @@ EXPORT_SYMBOL(vfs_fsync_range);
  */
 int vfs_fsync(struct file *file, int datasync)
 {
+#ifdef CONFIG_FS_SYNC
 	if (!fsync_enabled)
 		return 0;
-		
+#endif
+
 	return vfs_fsync_range(file, 0, LLONG_MAX, datasync);
 }
 EXPORT_SYMBOL(vfs_fsync);
@@ -201,9 +211,11 @@ static int do_fsync(unsigned int fd, int datasync)
 {
 	struct file *file;
 	int ret = -EBADF;
-	
+
+#ifdef CONFIG_FS_SYNC
 	if (!fsync_enabled)
 		return 0;
+#endif
 
 	file = fget(fd);
 	if (file) {
@@ -215,17 +227,21 @@ static int do_fsync(unsigned int fd, int datasync)
 
 SYSCALL_DEFINE1(fsync, unsigned int, fd)
 {
+#ifdef CONFIG_FS_SYNC
 	if (!fsync_enabled)
 		return 0;
-	
+#endif
+
 	return do_fsync(fd, 0);
 }
 
 SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
 {
+#ifdef CONFIG_FS_SYNC
 	if (!fsync_enabled)
 		return 0;
-		
+#endif
+
 	return do_fsync(fd, 1);
 }
 
@@ -239,9 +255,11 @@ SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
  */
 int generic_write_sync(struct file *file, loff_t pos, loff_t count)
 {
+#ifdef CONFIG_FS_SYNC
 	if (!fsync_enabled)
 		return 0;
-		
+#endif
+
 	if (!(file->f_flags & O_DSYNC) && !IS_SYNC(file->f_mapping->host))
 		return 0;
 	return vfs_fsync_range(file, pos, pos + count - 1,
@@ -306,8 +324,10 @@ SYSCALL_DEFINE(sync_file_range)(int fd, loff_t offset, loff_t nbytes,
 	int fput_needed;
 	umode_t i_mode;
 
+#ifdef CONFIG_FS_SYNC
 	if (!fsync_enabled)
 		return 0;
+#endif
 
 	ret = -EINVAL;
 	if (flags & ~VALID_FLAGS)
