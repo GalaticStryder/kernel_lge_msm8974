ab67b6d91f2424c186417d0e252edcec5d81667b
DerRomtester <dieb.stefan.96@gmail.com>
2015-01-07 15:14:07 +0100

    power: msm_battery: increase battery check interval from 5 to 15 seconds
    Signed-off-by: DerRomtester <dieb.stefan.96@gmail.com>

--- drivers/power/msm_battery.c
+++ drivers/power/msm_battery.c
@@ -67,7 +67,7 @@
 #define ONCRPC_CHG_GET_GENERAL_STATUS_PROC	12
 #define ONCRPC_CHARGER_API_VERSIONS_PROC	0xffffffff
 
-#define BATT_RPC_TIMEOUT    5000	/* 5 sec */
+#define BATT_RPC_TIMEOUT    15000	/* 15 sec */
 
 #define INVALID_BATT_HANDLE    -1
 
