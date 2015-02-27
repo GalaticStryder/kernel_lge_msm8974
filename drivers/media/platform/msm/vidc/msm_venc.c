dc948851a2b70c77e00d51169c64252fd0b8b3a5
Ícaro Hoff <icarohoff@gmail.com>
2015-02-26 19:28:36 -0800

    msm: vidc: revert all changes from CyanogenMod
    To better see your big yellow eyes...
    
    Revert "msm: vidc: replace resurrect_fw with unload_fw"
    
    This reverts commit b5f2ef312fa5462ca1aaebb6c9c83439cb0215f2.
    
    Revert "msm: vidc: Update the clock state variable before unprepare clocks"
    
    This reverts commit e95377275710c250a744adbd683412a0e897342f.
    
    Revert "msm: vidc: Handle power collapse cancel properly"
    
    This reverts commit 13f38b37fdb5f0abf55e2586797cbd0cf8ab2244.
    
    Revert "msm: vidc: Cancel pending power collapse works in core_release"
    
    This reverts commit 3f21a3e2aa3cfde23a7d9d3f23b59b9082506171.
    
    Revert "msm: vidc: Error handling when core is in bad state"
    
    This reverts commit 7f927214277d82e5bbb29ad5b7ef0567b09148a8.
    
    Revert "msm: vidc: Add support to read venus version through debugfs"
    
    This reverts commit f8a5724a05695838c377e7ee86e7ecc83fe55a07.
    
    Revert "msm: vidc: release sync_lock before waiting on command response"
    
    This reverts commit d6ec857beccefe8058e145a0e6c5b12929c33df9.
    
    Revert "msm: vidc: Clean up properly when instance exits at MSM_VIDC_INIT state"
    
    This reverts commit 0270ec02d8e75864c7aa0676db1949ec2da75319.
    
    Revert "msm: vidc: add driver support to enable operating rate"
    
    This reverts commit 587071fd56cf66d92c451eab650174afabb9318d.
    
    Revert "msm: vidc: Enable session priority support."
    
    This reverts commit 1336632b69912228925f5a9dccaa1e2e6938b963.
    
    Revert "msm: vidc: removed the async v4l2 event for session unsupported"
    
    This reverts commit f155a1672bfc76b04a8e1c09ad60a2b52a279a20.
    
    Revert "msm: vidc: kill unsupported sessions"
    
    This reverts commit 82f540cf4df1dac2ecb047c9c1c2ca5942a5498f.
    
    Revert "msm: vidc: Fix session error handling"
    
    This reverts commit 87882ef8955d3327e804c1d133c0acf71001e4c9.

--- drivers/media/platform/msm/vidc/msm_venc.c
+++ drivers/media/platform/msm/vidc/msm_venc.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -808,27 +808,7 @@ static struct msm_vidc_ctrl msm_venc_ctrls[] = {
 		.default_value = 1,
 		.step = 1,
 		.qmenu = NULL,
-	},
-	{
-		.id = V4L2_CID_MPEG_VIDC_VIDEO_PRIORITY,
-		.name = "Session Priority",
-		.type = V4L2_CTRL_TYPE_INTEGER,
-		.minimum = V4L2_MPEG_VIDC_VIDEO_PRIORITY_REALTIME_ENABLE,
-		.maximum = V4L2_MPEG_VIDC_VIDEO_PRIORITY_REALTIME_DISABLE,
-		.default_value = V4L2_MPEG_VIDC_VIDEO_PRIORITY_REALTIME_DISABLE,
-		.step = 1,
-		.qmenu = NULL,
-	},
-	{
-		.id = V4L2_CID_MPEG_VIDC_VIDEO_OPERATING_RATE,
-		.name = "Set Encoder Operating rate",
-		.type = V4L2_CTRL_TYPE_INTEGER,
-		.minimum = 0,
-		.maximum = 300 << 16,  /* 300 fps in Q16 format*/
-		.default_value = 0,
-		.step = 1,
-		.qmenu = NULL,
-	},
+	}
 };
 
 #define NUM_CTRLS ARRAY_SIZE(msm_venc_ctrls)
@@ -899,7 +879,6 @@ static struct msm_vidc_format venc_formats[] = {
 		.get_frame_size = get_frame_size_nv21,
 		.type = OUTPUT_PORT,
 	},
-
 };
 
 static int msm_venc_queue_setup(struct vb2_queue *q,
@@ -2223,14 +2202,6 @@ static int try_set_ctrl(struct msm_vidc_inst *inst, struct v4l2_ctrl *ctrl)
 		}
 		pdata = &hier_p_layers;
 		break;
-	case V4L2_CID_MPEG_VIDC_VIDEO_PRIORITY:
-		property_id = HAL_CONFIG_REALTIME;
-		enable.enable = ctrl->val;
-		pdata = &enable;
-		break;
-	case V4L2_CID_MPEG_VIDC_VIDEO_OPERATING_RATE:
-		property_id = 0;
-		break;
 	default:
 		dprintk(VIDC_ERR, "Unsupported index: %x\n", ctrl->id);
 		rc = -ENOTSUPP;
@@ -2835,14 +2806,6 @@ int msm_venc_prepare_buf(struct msm_vidc_inst *inst,
 
 	hdev = inst->core->device;
 
-	if (inst->state == MSM_VIDC_CORE_INVALID ||
-			inst->core->state == VIDC_CORE_INVALID) {
-		dprintk(VIDC_ERR,
-			"Core %p in bad state, ignoring prepare buf\n",
-				inst->core);
-		goto exit;
-	}
-
 	switch (b->type) {
 	case V4L2_BUF_TYPE_VIDEO_OUTPUT_MPLANE:
 		break;
@@ -2888,7 +2851,6 @@ int msm_venc_prepare_buf(struct msm_vidc_inst *inst,
 			"Buffer type not recognized: %d\n", b->type);
 		break;
 	}
-exit:
 	return rc;
 }
 
