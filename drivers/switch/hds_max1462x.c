4ea14262a67e44a474007d0e75c53598115133b2
Rashed Abdel-Tawab <rashed@linux.com>
2015-04-05 18:37:29 -0400

    Import D802 V30D kernel into LA.BF.1.1.1_RB1.05.00.02.042.013

--- drivers/switch/hds_max1462x.c
+++ drivers/switch/hds_max1462x.c
@@ -333,6 +333,7 @@ static void insert_headset(struct hsd_info *hi)
 	msleep(100);
 	HSD_DBG("insert delay additional 100\n");
 #endif
+
 	/* check if 3-pole or 4-pole
 	   1. read gpio_key
 	   2. check if 3-pole or 4-pole
