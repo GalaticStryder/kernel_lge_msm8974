ba6d12339619ff1f1cde0479c1e225aa2037a568
Deepak Kaushal <dkaushal@codeaurora.org>
2015-04-07 18:11:54 -0700

    msm: camera: Validate size param before allocating memory
    When ever i2c write is initiated check size param for NULL
    and in case of sequence write check for maximun allowed
    size per i2c sequence write.
    
    Change-Id: I111282537663d6b263a3686927c85b8f71560dae
    Signed-off-by: Deepak Kaushal <dkaushal@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor.c
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor.c
@@ -933,11 +933,13 @@ int msm_sensor_config(struct msm_sensor_ctrl_t *s_ctrl, void __user *argp)
 			write_config.slave_addr,
 			write_config.conf_array.size);
 
-		if (!write_config.conf_array.size) {
+		if (!write_config.conf_array.size ||
+			write_config.conf_array.size > I2C_REG_DATA_MAX) {
 			pr_err("%s:%d failed\n", __func__, __LINE__);
 			rc = -EFAULT;
 			break;
 		}
+
 		reg_setting = kzalloc(write_config.conf_array.size *
 			(sizeof(struct msm_camera_i2c_reg_array)), GFP_KERNEL);
 		if (!reg_setting) {
