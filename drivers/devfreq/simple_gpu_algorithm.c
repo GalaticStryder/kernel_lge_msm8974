030d88ec5656b76b29cd08d911f5d7b299fc4485
Dorimanx <yuri@bynet.co.il>
2015-05-02 20:20:04 +0300

    gpu: Add GPU OC up to 650Mhz - Beast!
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- drivers/devfreq/simple_gpu_algorithm.c
+++ drivers/devfreq/simple_gpu_algorithm.c
@@ -26,24 +26,33 @@ module_param_named(simple_ramp_threshold, ramp_up_threshold, int, 0664);
 int simple_gpu_active = 0;
 module_param_named(simple_gpu_activate, simple_gpu_active, int, 0664);
 
+int simple_gpu_debug = 0;
+module_param_named(simple_gpu_debug, simple_gpu_debug, int, 0664);
+
 static int laziness;
 
-int simple_gpu_algorithm(int level,
-			struct devfreq_msm_adreno_tz_data *priv)
+int simple_gpu_algorithm(int level, struct devfreq_msm_adreno_tz_data *priv)
 {
 	int val = 0;
+
+	if (simple_gpu_debug == 1)
+		pr_info("simple_gpu_debug: level is %d\n", level);
+
+	if (simple_gpu_debug == 2)
+		pr_info("simple_gpu_debug: busy_time is %u\n", (unsigned int)priv->bin.busy_time);
+
+	if (simple_gpu_debug == 3)
+		pr_info("simple_gpu_debug: bus.num is %d\n", priv->bus.num + 2);
 
 	/* it's currently busy */
 	if ((unsigned int)priv->bin.busy_time > ramp_up_threshold) {
 		if (level == 0)
 			val = 0; /* already maxed, so do nothing */
-		else if ((level > 0) &&
-			(level <= (priv->bus.num - 1)))
+		else if ((level > 0) && (level <= (priv->bus.num + 2)))
 			val = -1; /* bump up to next pwrlevel */
 	/* idle case */
 	} else {
-		if ((level >= 0) &&
-			(level < (priv->bus.num - 1)))
+		if ((level >= 0) && (level < (priv->bus.num + 2)))
 			if (laziness > 0) {
 				/* hold off for a while */
 				laziness--;
@@ -52,9 +61,12 @@ int simple_gpu_algorithm(int level,
 				val = 1; /* above min, lower it */
 				/* reset laziness count */
 				laziness = default_laziness;
-		} else if (level == (priv->bus.num - 1))
+		} else if (level == (priv->bus.num + 2))
 			val = 0; /* already @ min, so do nothing */
 	}
+	if (simple_gpu_debug == 4)
+		pr_info("simple_gpu_debug: returning val is %d\n", val);
+
 	return val;
 }
 EXPORT_SYMBOL(simple_gpu_algorithm);
