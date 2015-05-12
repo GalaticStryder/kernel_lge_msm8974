5be66aca4458450599b3d6d2c08f8853c63ade59
andip71 <andreasp@gmx.de>
2015-05-12 13:58:02 +0200

    cpufreq: Add nightmare governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -417,6 +417,9 @@ extern struct cpufreq_governor cpufreq_gov_intellimm;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
 extern struct cpufreq_governor cpufreq_gov_lionheart;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_LIONHEART)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
+extern struct cpufreq_governor cpufreq_gov_nightmare;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_nightmare)
 #endif
 
 
