66fc17da6567718171e3ec41a94a19f048aa3b43
franciscofranco <franciscofranco.1990@gmail.com>
2015-12-03 23:14:42 +0000

    wakeup: add toggles for wlan wakelocks.
    They are all enabled by default, it's up to the user and I provide no support if Wi-Fi stops working normally without these locks enabled. This is for advanced users
    
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -15,6 +15,14 @@
 #include <linux/seq_file.h>
 #include <linux/debugfs.h>
 #include <trace/events/power.h>
+#include <linux/moduleparam.h>
+
+static bool enable_wlan_rx_wake_ws = true;
+module_param(enable_wlan_rx_wake_ws, bool, 0644);
+static bool enable_wlan_ctrl_wake_ws = true;
+module_param(enable_wlan_ctrl_wake_ws, bool, 0644);
+static bool enable_wlan_wake_ws = true;
+module_param(enable_wlan_wake_ws, bool, 0644);
 
 #include "power.h"
 /* #define POLL_DEBUG */
@@ -423,6 +431,16 @@ static void wakeup_source_activate(struct wakeup_source *ws)
 		}
 	}
 #endif
+
+	if (!enable_wlan_rx_wake_ws && !strcmp(ws->name, "wlan_rx_wake"))
+                return;
+
+	if (!enable_wlan_ctrl_wake_ws && !strcmp(ws->name, "wlan_ctrl_wake"))
+                return;
+
+	if (!enable_wlan_wake_ws && !strcmp(ws->name, "wlan_wake"))
+                return;
+
 	ws->active = true;
 	ws->active_count++;
 	ws->last_time = ktime_get();
