bb3945ee144e624f9a4a9f5482374a4c72adbd77
Zachariah <zkennedy87@gmail.com>
2016-02-10 22:27:01 +0000

    SmartMAX: Import and setup for N6!
    * Converted to use PowerSuspend
    * Removed Boosting, we use CPU-Boost
    
    Signed-off-by: Zachariah <zkennedy87@gmail.com>
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -455,6 +455,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
 extern struct cpufreq_governor cpufreq_gov_impulse;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
+extern struct cpufreq_governor cpufreq_gov_smartmax;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartmax)
 #endif
 
 /*********************************************************************
