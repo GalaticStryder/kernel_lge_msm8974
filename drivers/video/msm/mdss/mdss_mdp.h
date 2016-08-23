e211463dac7aacb7050158db53b6682a33d3885b
Ícaro Hoff <icarohoff@gmail.com>
2016-08-23 11:20:20 -0300

    mdss: adjust and cleanup kickoff timeout
    The 'mdss_mdp_intf_cmd.c' already includes the same definition of
    KOFF_TIMEOUT from 'mdss_mdp.h'.
    Remove that dupicate definition and cleanup comment.
    
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- drivers/video/msm/mdss/mdss_mdp.h
+++ drivers/video/msm/mdss/mdss_mdp.h
@@ -54,8 +54,7 @@
 #define C1_B_Cb		1	/* B/Cb */
 #define C0_G_Y		0	/* G/luma */
 
-/* wait for 300ms to take into account scheduling related delays
- * This number is empirical*/
+/* Wait for 300ms to take into account scheduling related delays. */
 #define KOFF_TIMEOUT msecs_to_jiffies(300)
 
 #define OVERFETCH_DISABLE_TOP		BIT(0)
