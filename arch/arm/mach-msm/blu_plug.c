58938d080a17a91c808b07e253ccaba8cc70ae74
Ícaro Hoff <icarohoff@gmail.com>
2016-08-27 10:37:15 -0300

    blu_plug: add switcher to enable/disable hotplugging
    As I imported it directly from engstk, it doesn't have the ability to
    enable/disable when we want because he only uses his hotplug at his
    kernel.
    I use bricked as main hotplug and don't want to stop using it.
    Adding a switcher to workaround this...
    
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- arch/arm/mach-msm/blu_plug.c
+++ arch/arm/mach-msm/blu_plug.c
@@ -25,6 +25,7 @@
 #include <linux/delay.h>
 #include <linux/slab.h>
 
+#define BLU_PLUG_ENABLED 0
 #define INIT_DELAY		(60000) /* Initial delay to 60 sec, 4 cores while boot */
 #define DELAY			(500)
 #define UP_THRESHOLD		(90)
@@ -35,7 +36,8 @@
 #define MAX_CORES_SCREENOFF (2)
 #define DEF_PLUG_THRESHOLD 0
 
-static unsigned int up_threshold = UP_THRESHOLD;;
+static unsigned int blu_plug_enabled = BLU_PLUG_ENABLED;
+static unsigned int up_threshold = UP_THRESHOLD;
 static unsigned int delay = DELAY;
 static unsigned int min_online = MIN_ONLINE;
 static unsigned int max_online = MAX_ONLINE;
@@ -392,8 +394,12 @@ module_param_array(plug_threshold, uint, NULL, 0644);
 
 /***************** end of module parameters *****************/
 
-static int __init dyn_hp_init(void)
+static int dyn_hp_init(void)
 {
+	if (!blu_plug_enabled) {
+		return 0;
+		pr_info("%s: disabled\n", __func__);
+	}
 	notify.notifier_call = fb_notifier_callback;
 	if (fb_register_client(&notify) != 0)
 		pr_info("%s: Failed to register FB notifier callback\n", __func__);
@@ -412,7 +418,7 @@ static int __init dyn_hp_init(void)
 	return 0;
 }
 
-static void __exit dyn_hp_exit(void)
+static void dyn_hp_exit(void)
 {
 	cancel_delayed_work_sync(&dyn_work);
 	fb_unregister_client(&notify);
@@ -420,6 +426,37 @@ static void __exit dyn_hp_exit(void)
 	
 	pr_info("%s: deactivated\n", __func__);
 }
+
+static int set_enabled(const char *val, const struct kernel_param *kp)
+{
+	int ret = 0;
+	unsigned int i;
+	int blu = 0;
+
+	ret = kstrtouint(val, 10, &i);
+	if (ret)
+		return -EINVAL;
+	if (i < 0 || i > 1)
+		return 0;
+		
+	if (i == blu_plug_enabled)
+		return i;
+
+	ret = param_set_uint(val, kp);
+	blu_plug_enabled = i;
+	if ((blu_plug_enabled == 1))
+		blu = dyn_hp_init();
+	if ((blu_plug_enabled == 0))
+		dyn_hp_exit();
+	return i;
+}
+
+static struct kernel_param_ops enabled_ops = {
+	.set = set_enabled,
+	.get = param_get_uint,
+};
+
+module_param_cb(enabled, &enabled_ops, &blu_plug_enabled, 0644);
 
 MODULE_AUTHOR("Stratos Karafotis <stratosk@semaphore.gr");
 MODULE_AUTHOR("engstk <eng.stk@sapo.pt>");
