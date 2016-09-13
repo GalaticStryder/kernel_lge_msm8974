1f2ba794d0285072090f90733e15a361d44d60de
Pranav Vashi <neobuddy89@gmail.com>
2016-09-13 16:47:56 +0530

    blu_plug: Convert direct references to HZ
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- arch/arm/mach-msm/blu_plug.c
+++ arch/arm/mach-msm/blu_plug.c
@@ -28,15 +28,15 @@
 #include <linux/slab.h>
 
 #define BLU_PLUG_ENABLED 0
-#define INIT_DELAY		(60000) /* Initial delay to 60 sec, 4 cores while boot */
-#define DELAY			(500)
-#define UP_THRESHOLD		(90)
-#define MIN_ONLINE		(2)
-#define MAX_ONLINE		(4)
-#define DEF_DOWN_TIMER_CNT	(6)	/* 3 secs */
-#define DEF_UP_TIMER_CNT	(2)	/* 1 sec */
-#define MAX_CORES_SCREENOFF (2)
-#define DEF_PLUG_THRESHOLD 0
+#define INIT_DELAY		20000
+#define DELAY			100
+#define UP_THRESHOLD		80
+#define MIN_ONLINE		1
+#define MAX_ONLINE		4
+#define DEF_DOWN_TIMER_CNT	6
+#define DEF_UP_TIMER_CNT	2
+#define MAX_CORES_SCREENOFF 1
+#define DEF_PLUG_THRESHOLD 70
 
 static unsigned int blu_plug_enabled = BLU_PLUG_ENABLED;
 static unsigned int up_threshold = UP_THRESHOLD;
