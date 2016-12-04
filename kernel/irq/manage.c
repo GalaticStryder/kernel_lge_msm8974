44efd286f4a37a3af3e2dbf7bfed7c71899269a0
Dorimanx <yuri@bynet.co.il>
2016-12-04 14:43:27 +0200

    msm: sps: add tunable to allow IRQ suspend on the fly
    Restore deepsleep usecase for modem data connection as implemented by LGE.
    
    Parameter:
    /sys/module/sps_bam/parameters/allow_suspend
    
    Set to 1 to allow suspending IRQ to save power, setting to 0 will not
    let modem get into deep sleep on data connection.
    
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- kernel/irq/manage.c
+++ kernel/irq/manage.c
@@ -451,7 +451,9 @@ void __enable_irq(struct irq_desc *desc, unsigned int irq, bool resume)
 	switch (desc->depth) {
 	case 0:
  err_out:
-		WARN(1, KERN_WARNING "Unbalanced enable for IRQ %d\n", irq);
+		/* Ignore IRQ 61 SPS warnings (modem data wakeup) */
+		if (irq != 61)
+			WARN(1, KERN_WARNING "Unbalanced enable for IRQ %d\n", irq);
 		break;
 	case 1: {
 		if (desc->istate & IRQS_SUSPENDED)
