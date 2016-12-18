66ef3b97eb228f01b9d0f6a5fc251e18f2260741
Abhijit Kulkarni <kabhijit@codeaurora.org>
2016-12-18 17:36:48 +0100

    msm: mdss: hide kernel addresses from unprevileged users
    for printing kernel pointers which should be hidden from unprivileged
    users, use %pK which evaluates whether kptr_restrict is set.
    
    CRs-Fixed: 987021
    Signed-off-by: Abhijit Kulkarni <kabhijit@codeaurora.org>
    Signed-off-by: Nirmal Abraham <nabrah@codeaurora.org>
    Signed-off-by: Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
    (cherry picked from commit fbcd783)
    
    Change-Id: Iac3e6df8424c6790f178b69be56a3d1f41a2365a
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -612,7 +612,7 @@ static int mdss_dsi_panel_partial_update(struct mdss_panel_data *pdata)
 				panel_data);
 	mipi  = &pdata->panel_info.mipi;
 
-	pr_debug("%s: ctrl=%p ndx=%d\n", __func__, ctrl, ctrl->ndx);
+	pr_debug("%s: ctrl=%pK ndx=%d\n", __func__, ctrl, ctrl->ndx);
 
 	caset[1] = (((pdata->panel_info.roi_x) & 0xFF00) >> 8);
 	caset[2] = (((pdata->panel_info.roi_x) & 0xFF));
@@ -749,7 +749,7 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 				panel_data);
 	mipi  = &pdata->panel_info.mipi;
 
-	pr_info("%s+: ctrl=%p ndx=%d\n", __func__, ctrl, ctrl->ndx);
+	pr_info("%s+: ctrl=%pK ndx=%d\n", __func__, ctrl, ctrl->ndx);
 
 	if (ctrl->on_cmds.cmd_cnt)
 		mdss_dsi_panel_cmds_send(ctrl, &ctrl->on_cmds);
@@ -771,7 +771,7 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
 
-	pr_info("%s+: ctrl=%p ndx=%d\n", __func__, ctrl, ctrl->ndx);
+	pr_info("%s+: ctrl=%pK ndx=%d\n", __func__, ctrl, ctrl->ndx);
 
 	mipi  = &pdata->panel_info.mipi;
 
