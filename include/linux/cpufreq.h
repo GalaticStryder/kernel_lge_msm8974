f5be249027d28327c971e4e214235d09bff7d2b8
alucard24 <dmbaoh2@gmail.com>
2016-12-29 11:36:52 -0200

    cpufreq: import and setup nightmare governor
    Just to add here, a lot this one and the previous governor came from
    dorimanx as it's tuning fits better on this device.
    
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -461,6 +461,9 @@ extern struct cpufreq_governor cpufreq_gov_smartmax;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
 extern struct cpufreq_governor cpufreq_gov_darkness;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
+extern struct cpufreq_governor cpufreq_gov_nightmare;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_nightmare)
 #endif
 
 /*********************************************************************
